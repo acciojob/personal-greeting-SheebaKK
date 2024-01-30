f76b24458d0909f19b640e4e076efcbb7af8d2dc
rahgeer2021 <rahgeer2021@gmail.com>
2024-01-30 17:23:48 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [name,setName]=useState("");
+
+  const handleChange =(e)=>{
+
+    setName(e.target.value);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      Enter your name:<br />
+        <input type="text" onChange={handleChange} value={name}/>
+        <br />
+        {name ? <p>Hello {name}</p>:""}
     </div>
   )
 }
